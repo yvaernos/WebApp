8570f07e327d947bad586f5fd5a217d7f633e447
yvaernos <45668447+yvaernos@users.noreply.github.com>
2021-04-12 17:38:39 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.htm', function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('File not found')
+        }else{
+            res.write(data)
+        }
+        res.end()
+    })
+
+
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Something wrong')
+    }else{
+        console.log('listening to port' + port)
+    }
+})
