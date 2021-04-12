701984942fe8478021594f84269e2991bb421b38
yvaernos <45668447+yvaernos@users.noreply.github.com>
2021-04-12 17:41:40 +0100

    update1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const fs = require('fs')
-const port = 4000
+const port = 3000
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'})
