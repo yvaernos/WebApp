197b943ac55d98c8f700a4c58f8fdcadd5d59135
yvaernos <45668447+yvaernos@users.noreply.github.com>
2021-05-04 16:16:40 +0100

    Fixing typo on app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const productRouter = require('./api/routes/products')
 //Middleware
 app.use(express.json())
 
-// app.use((req, res, next) =>{
-//     res.status(200).json({
-//         message: 'all good'
-//     })
-// })
+app.use((req, res, next) =>{
+    res.status(200).json({
+        message: 'all good'
+    })
+})
 
 //CORS errors handling
 app.use((req, res, next) =>{
