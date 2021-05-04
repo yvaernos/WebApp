8a03cd76cbaec6ba86c780b105f7e4eab7145ff9
yvaernos <45668447+yvaernos@users.noreply.github.com>
2021-05-04 16:33:36 +0100

    Fixing issues with missing mongodb key

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
